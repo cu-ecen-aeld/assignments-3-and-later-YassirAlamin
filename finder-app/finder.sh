75f58e1da53c6c6a3a0c0ccb1599e530d997d916
YassirAlamin <yassiralamin@gmail.com>
2023-11-11 17:41:13 +0400

    finder.sh and writer.sh are added

---
+++ finder-app/finder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+filedir=$1
+searchstr=$2
+
+
+if [ $# != 2 ]
+then
+	echo "Input Parameters Error"
+	exit 1
+fi
+
+if [ ! -d $filedir  ]; then
+	echo "Parameter 2 is not a directory"
+	exit 1
+fi
+
+# Matches lines
+Y=$(grep -r $searchstr $filedir | wc -l)
+
+# Number of files 
+X=$(grep -rl $searchstr $filedir | wc -l)
+
+echo "\nThe number of files are" $X "and the number of matching lines are" $Y
+
---
+++ finder-app/writer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+writefile=$1
+writestr=$2
+
+if [ $# != 2 ]
+then
+	echo "Input prameter error"
+	exit 1
+fi
+
+path=$(dirname $writefile)
+mkdir -p $path
+
+echo $writestr > $writefile
+
+if [ ! -f $writefile ] 
+then
+	echo "File name Error"
+	exit 1
+fi
+
+
+if [ ! -e $writefile ]
+then
+	echo "file can not created"
+	exit 1
+fi
