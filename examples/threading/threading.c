e55d966f57a62372dca6ddde87f4210d80948fc3
YassirAlamin <yassiralamin@gmail.com>
2024-02-20 16:59:38 +0400

    assignment 4 part 1 submit #1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,8 +13,25 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    // struct thread_data* thread_func_args = (struct thread_data *) thread_param; 
+	
+	
+	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+	
+	usleep(thread_func_args->wait_to_obtain_ms*1000);
+	if(pthread_mutex_lock(thread_func_args->mutex)){
+		printf("Lock error\r\n");
+		return thread_param;
+	}
+	usleep(thread_func_args->wait_to_release_ms*1000);
+	
+	if(pthread_mutex_unlock(thread_func_args->mutex)){printf("unlock Error\r\n"); return thread_param;}
+	
+	thread_func_args->thread_complete_success = true;
+	printf("return\r\n");
+    
+	return thread_param;
 }
 
 
@@ -28,6 +45,22 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+	//pthread_t thrd;
+
+	struct thread_data *Inptr = (struct thread_data *) malloc(sizeof(struct thread_data));
+	//struct thread_data *Outptr;
+
+	if(Inptr == NULL){printf("malloc Error\r\n"); return false;}
+
+//	pthread_mutex_init(mutex, NULL);	
+	
+	Inptr->mutex = mutex;
+	Inptr->wait_to_obtain_ms = wait_to_obtain_ms;
+	Inptr->wait_to_release_ms = wait_to_release_ms;
+	Inptr->thread_complete_success = false;
+	
+	if((pthread_create(thread, NULL, threadfunc, (void*)Inptr))){printf("pthread_create Error\r\n"); free(Inptr); return false;}
+
+    return true;
 }
 
